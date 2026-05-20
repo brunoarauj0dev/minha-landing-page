bfcb1600960ee37d46a0f6cdb1796c6828d84358
Bruno Araujo <bruninhoaraujo83@gmail.com>
2026-05-20 17:55:53 -0300

    adiocionando animaçãoes e scroll

--- js/script.js
+++ js/script.js
@@ -4,3 +4,17 @@ const menu = document.getElementById('menu');
 hamburguer.addEventListener('click', function() {
   menu.classList.toggle('ativo');
 });
+
+const elementos = document.querySelectorAll('.animar');
+
+const observer = new IntersectionObserver(function(entries) {
+  entries.forEach(function(entry) {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visivel');
+    }
+  });
+});
+
+elementos.forEach(function(elemento) {
+  observer.observe(elemento);
+});
